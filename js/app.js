f111d5ec2cf6a4d12e59c967b70b6cf0fac0f335
santosvini7 <vini_santos7@live.com>
2023-05-04 19:30:20 -0300

    ✨ 🎨 add favicon, correcoes de box-shadow, transicao suave entre secoes

--- js/app.js
+++ js/app.js
@@ -1,41 +1,72 @@
 $('.carousel').carousel({
-  interval: 2000
+  interval: 3000
 })
 
-const form = document.querySelector('#form')
-const name = document.querySelector('#name')
-const number = document.querySelector('#number')
-const email = document.querySelector('#email')
-const address = document.querySelector('#address')
-const codeZip = document.querySelector('#codeZip')
-const textarea = document.querySelector('#validationTextarea')
+function hideNav() {
+  $(".nav-link").on("click", function(){
+    $('.navbar-collapse').collapse('hide');
+  });
+}
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
+function submitForm() {
+  
+  const form = document.querySelector('#form')
+  const name = document.querySelector('#name')
+  const number = document.querySelector('#number')
+  const email = document.querySelector('#email')
+  const address = document.querySelector('#address')
+  const codeZip = document.querySelector('#codeZip')
+  const textarea = document.querySelector('#validationTextarea')
 
-  if (name.value === "") {
-    alert('Favor, preencha o nome')
-    return; 
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    if (name.value === "") {
+      alert('Favor, preencha o nome')
+      return; 
+    }
+    if (number.value === "") {
+      alert('Favor, preencha o celular')
+      return; 
+    }
+    if (email.value === "") {
+      alert('Favor, preencha o email')
+      return; 
+    }
+    if (address.value === "") {
+      alert('Favor, preencha o endereço')
+      return; 
+    }
+    if (codeZip.value === "") {
+      alert('Favor, preencha o cep')
+      return; 
+    }
+    if (textarea.value === "") {
+      alert('Favor, preencha o seu projeto')
+      return; 
+    }
+    form.submit();
+  });
+}
+
+function moveSection() {
+  const section = document.querySelectorAll('.nav-item a[href^="#"]')
+
+  function transiton(event) {
+    event.preventDefault();
+
+    const href = event.currentTarget.getAttribute('href');
+    const links = document.querySelector(href);
+    links.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    })
   }
-  if (number.value === "") {
-    alert('Favor, preencha o celular')
-    return; 
-  }
-  if (email.value === "") {
-    alert('Favor, preencha o email')
-    return; 
-  }
-  if (address.value === "") {
-    alert('Favor, preencha o endereço')
-    return; 
-  }
-  if (codeZip.value === "") {
-    alert('Favor, preencha o cep')
-    return; 
-  }
-  if (textarea.value === "") {
-    alert('Favor, preencha o seu projeto')
-    return; 
-  }
-  form.submit();
-});
+  section.forEach((link) => {
+    link.addEventListener('click', transiton)
+  })
+}
+
+hideNav();
+submitForm();
+moveSection();
